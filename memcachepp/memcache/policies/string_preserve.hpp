da7a193166dc3c81224c27116ebac100345f5729
Dean Michael Berris <mikhailberis@gmail.com>
2010-10-06 22:22:23 +0800

    Fixing ADL failure on operator>> and operator<< overloads, standards compliance for clang builds.

--- memcachepp/memcache/policies/string_preserve.hpp
+++ memcachepp/memcache/policies/string_preserve.hpp
@@ -15,22 +15,46 @@
 
 namespace memcache { namespace policies {
 
+    namespace detail {
+
+        struct oarchive {
+            explicit oarchive(std::ostringstream & os)
+                : _os(os)
+                { };
+            std::ostringstream & _os;
+        };
+        
+        struct iarchive {
+            explicit iarchive(std::istream & is)
+                : _is(is)
+                { };
+            std::istream & _is;
+        };
+
+        template <class String>
+        inline oarchive & operator<< ( oarchive & archive, String const & str) {
+            archive._os.str(str);
+            return archive;
+        };
+
+        template <class String>
+        inline iarchive & operator>> ( iarchive & archive, String & str) {
+            std::stringbuf buffer;
+            while (!archive._is.eof()) {
+                archive._is.get(buffer);
+                if (archive._is.peek() == '\n')
+                    buffer.sputc(archive._is.get());
+            };
+            str = buffer.str();
+            return archive;
+        };
+
+    } // namespace detail
+
     template <class tag = tags::default_tag>
         struct string_preserve_impl {
-            struct oarchive {
-                explicit oarchive(std::ostringstream & os)
-                    : _os(os)
-                    { };
-                std::ostringstream & _os;
-            };
-            
-            struct iarchive {
-                explicit iarchive(std::istream & is)
-                    : _is(is)
-                    { };
-                std::istream & _is;
-            };
-
+            typedef detail::oarchive oarchive;
+            typedef detail::iarchive iarchive;
             typedef std::runtime_error archive_exception;
 
             protected: ~string_preserve_impl() { };
@@ -39,24 +63,6 @@ namespace memcache { namespace policies {
     typedef string_preserve_impl<> string_preserve;
 
 }; // namespace policies
-
-    template <class String>
-    inline  policies::string_preserve::oarchive & operator<< ( policies::string_preserve::oarchive & archive, String const & str) {
-        archive._os.str(str);
-        return archive;
-    };
-
-    template <class String>
-    inline  policies::string_preserve::iarchive & operator>> ( policies::string_preserve::iarchive & archive, String & str) {
-        std::stringbuf buffer;
-        while (!archive._is.eof()) {
-            archive._is.get(buffer);
-            if (archive._is.peek() == '\n')
-                buffer.sputc(archive._is.get());
-        };
-        str = buffer.str();
-        return archive;
-    };
 
 }; // namespace memcache
 
